2a206da7a7fd81bbe6442b21fc4dc954883b721c
Steffen Kühne <stekhn@gmail.com>
2021-01-11 23:34:53 +0100

    Add waveform effect

--- js/player.js
+++ js/player.js
@@ -1,14 +1,15 @@
-"use strict";
+'use strict';
 
-document.addEventListener("DOMContentLoaded", loadData, false);
+document.addEventListener('DOMContentLoaded', loadData, false);
 
-var data = {};
-var player = {};
+var storyData;
+var player;
+var wave;
 var elements = {};
 
 function loadData() {
   // Load story data
-  fetch("./js/story.json")
+  fetch('./js/story.json')
     .then(function (response) {
       return response.json();
     })
@@ -20,55 +21,59 @@ function loadData() {
 function setupPage(data) {
   storyData = data;
 
-  elements.startButton = document.querySelector("#start");
-  elements.content = document.querySelector("#content");
-  elements.player = document.querySelector("#player");
+  elements.startButton = document.querySelector('#start');
+  elements.content = document.querySelector('#content');
+  elements.wave = document.querySelector('#wave');
+  elements.player = document.querySelector('#player');
 
-  elements.startButton.addEventListener("click", function () {
+  elements.startButton.addEventListener('click', function () {
     var introObject = storyData.filter(function (storyObject) {
-      return storyObject.id === "intro";
+      return storyObject.id === 'intro';
     })[0];
+    
+    elements.player.style.display = 'block';
+
+    setupWave();
     updatePage(introObject);
   });
 
-  elements.playButton = document.querySelector("#play");
-  elements.pauseButton = document.querySelector("#pause");
-  elements.skipLeftButton = document.querySelector("#skip-left");
-  elements.skipRightButton = document.querySelector("#skip-right");
+  elements.playButton = document.querySelector('#play');
+  elements.pauseButton = document.querySelector('#pause');
+  elements.skipLeftButton = document.querySelector('#skip-left');
+  elements.skipRightButton = document.querySelector('#skip-right');
 
-  elements.playButton.addEventListener("click", startPlayer);
-  elements.pauseButton.addEventListener("click", pausePlayer);
-  elements.skipLeftButton.addEventListener("click", skipLeftPlayer);
-  elements.skipRightButton.addEventListener("click", skipRightPlayer);
+  elements.playButton.addEventListener('click', startPlayer);
+  elements.pauseButton.addEventListener('click', pausePlayer);
+  elements.skipLeftButton.addEventListener('click', skipLeftPlayer);
+  elements.skipRightButton.addEventListener('click', skipRightPlayer);
 }
 
 function updatePage(storyObject) {
   updateContent(storyObject);
   updatePlayer(storyObject);
-  elements.player.style.display = "block";
 }
 
 function updateContent(storyObject) {
-  elements.content.innerHTML = "";
+  elements.content.innerHTML = '';
 
-  var h1 = document.createElement("h1");
+  var h1 = document.createElement('h1');
   h1.textContent = storyObject.title;
   elements.content.appendChild(h1);
 
-  var p = document.createElement("p");
+  var p = document.createElement('p');
   p.textContent = storyObject.text;
   elements.content.appendChild(p);
 
-  var optionsWrapper = document.createElement("p");
-  optionsWrapper.className = "options";
+  var optionsWrapper = document.createElement('p');
+  optionsWrapper.className = 'options';
   elements.content.appendChild(optionsWrapper);
 
   storyObject.options.forEach(function (option) {
-    var optionButton = document.createElement("button");
+    var optionButton = document.createElement('button');
     optionButton.textContent = option.description;
     optionsWrapper.appendChild(optionButton);
 
-    optionButton.addEventListener("click", function () {
+    optionButton.addEventListener('click', function () {
       var storyObject = storyData.filter(function (storyObject) {
         return storyObject.id === option.link;
       })[0];
@@ -90,24 +95,27 @@ function updatePlayer(storyObject) {
 }
 
 function startPlayer() {
-  elements.playButton.style.display = "none";
-  elements.pauseButton.style.display = "inline-block";
+  elements.playButton.style.display = 'none';
+  elements.pauseButton.style.display = 'inline-block';
 
   player.play();
+  wave.start();
 }
 
 function pausePlayer() {
-  elements.playButton.style.display = "inline-block";
-  elements.pauseButton.style.display = "none";
+  elements.playButton.style.display = 'inline-block';
+  elements.pauseButton.style.display = 'none';
 
   player.pause();
+  wave.stop();
 }
 
 function resetPlayer() {
-  elements.playButton.style.display = "inline-block";
-  elements.pauseButton.style.display = "none";
+  elements.playButton.style.display = 'inline-block';
+  elements.pauseButton.style.display = 'none';
 
   player.stop();
+  wave.stop();
 }
 
 function skipLeftPlayer() {
@@ -121,3 +129,34 @@ function skipRightPlayer() {
 
   player.seek(currentPosition + 15);
 }
+
+function setupWave() {
+  wave = new SiriWave({
+    container: elements.wave,
+    width: window.innerWidth,
+    height: window.innerHeight * 0.3,
+    cover: true,
+    speed: 0.03,
+    amplitude: 0.7,
+    frequency: 2
+  });
+
+  window.addEventListener('resize', updateWave);
+
+  updateWave();
+}
+
+function updateWave() {
+  var height = window.innerHeight * 0.3;
+  var width = window.innerWidth;
+
+  wave.height = height;
+  wave.height_2 = height / 2;
+  wave.MAX = wave.height_2 - 4;
+  wave.width = width;
+  wave.width_2 = width / 2;
+  wave.width_4 = width / 4;
+  wave.canvas.height = height;
+  wave.canvas.width = width;
+};
+  
